5d07409b21e66c6563c5a2f052f3c35c1c0b8535
tamperic <tamaraperic.work@gmail.com>
2025-06-04 14:40:26 +0200

    Implement AsyncStorage in Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
 import { Send, Bubble, GiftedChat, InputToolbar, Composer, SystemMessage, Day } from "react-native-gifted-chat"; // Import Gifted Chat library
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { addDoc, collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage"; // A persistent key-value storage mechanism in which can strings be stored.
 
 
 const Chat = ({ route, navigation, db, isConnected }) => {
@@ -13,26 +14,55 @@ const Chat = ({ route, navigation, db, isConnected }) => {
         // Update 'onSend' function to save sent messages on the Firestore database.
         addDoc(collection(db, 'messages'), newMessages[0]); // Use the 'addDoc()' Firestore function to save the passed message to the function in the database.
     };
-    
+        
+
+    // Cache all messages whenever they’re updated. 
+    const cacheMessages = async (messagesToCache) => {
+        try {
+            // Convert objects and arrays into strings to store them in 'AsyncStorage'.
+            await AsyncStorage.setItem('messages', JSON.stringify(messagesToCache));
+        } catch (error) {
+            console.log(error.messages);
+        }
+    }
+
+    const loadCachedMessages = async () => {
+        const cachedMessages = await AsyncStorage.getItem("messages") || [];
+        setMessages(JSON.parse(cachedMessages));
+    }
+
+
+    let unsubMessages;// Declare the 'unsubMessages' variable outside the 'useEffect()' to not to lose the reference to the old unsubscribe function. 
+    // This is because re-initializing any kind of listener doesn’t override and remove the old listener, it only removes the reference to that old listener, while keeping it alive somewhere in the memory without a way of reaching it, resulting in a memory leak.
+
+    // 'onSnapshot()' takes a “snapshot” of your collection and its documents at the precise moment that it’s called.
     useEffect(() => {
         navigation.setOptions({ title: name });
 
-        // code to execute when component mounted or updated
-        const q = query(collection(db, "messages"), orderBy('createdAt', 'desc'));
-        const unsubChat = onSnapshot(q, (documentsSnapshot) => {
-            let newMessages = []; // An empty array (newMessages) is created, which will be filled later in the 'forEach()' loop.
-            documentsSnapshot.forEach(docObject => {
-                newMessages.push({id: docObject.id, ...docObject.data(), createdAt: docObject.data().createdAt.toDate() });
-            }); // First the document id found in the '.id' property of each object within this complex object. The actual document properties can be extracted with the '.data()' function of each object in 'messages'. And the TimeStamp stored at the 'createdAt' property of each message is converted to a Date object that Gifted Chat understands.
-            setMessages(newMessages); // Use the state setter function 'setMessages' to assign the new array to 'messages' state.
-        });
+        if (isConnected === true) {
+            // Unregister current 'onSnapshot()' listener to avoid registering multiple listeners when 'useEffect' code is re-executed.
+            if (unsubMessages) unsubMessages();
+            unsubMessages = null;
+
+            // code to execute when component mounted or updated
+            const q = query(collection(db, "messages"), orderBy('createdAt', 'desc'));
+            unsubMessages = onSnapshot(q, async (documentsSnapshot) => {
+                let newMessages = []; // An empty array (newMessages) is created, which will be filled later in the 'forEach()' loop.
+                documentsSnapshot.forEach(docObject => {
+                    newMessages.push({id: docObject.id, ...docObject.data(), createdAt: docObject.data().createdAt.toDate() });
+                }); // First the document id found in the '.id' property of each object within this complex object. The actual document properties can be extracted with the '.data()' function of each object in 'messages'. And the TimeStamp stored at the 'createdAt' property of each message is converted to a Date object that Gifted Chat understands.
+                cacheMessages(newMessages);
+                setMessages(newMessages); // Use the state setter function 'setMessages' to assign the new array to 'messages' state.
+            });
+        } else loadCachedMessages();
 
         // Clean up code (returned function) by calling the unsubsrcibe function of 'onSnapshot()'
         return () => {
             // code to execute when the component will be unmounted
-            if (unsubChat) unsubChat(); // An 'if' statement has been added to check if the 'unsubChat' isn't undefined. This is a protection procedure just in case the 'onSnapshot()' function call fails.
+            if (unsubMessages) unsubMessages(); // An 'if' statement has been added to check if the 'unsubMessages' isn't undefined. This is a protection procedure just in case the 'onSnapshot()' function call fails.
         }
-    }, []); // 'useEffect()' function gets called right after the Chat component mounts and sets up an initial default message in the chat (A message saying "Hello developer" from a user named "React Native" with an avatar.)
+    }, [isConnected]); // Allow the component to call the callback of 'useEffect' whenever the 'isConnected' prop’s value changes.
+
 
 
     const renderBubble = (props) => {
@@ -54,19 +84,25 @@ const Chat = ({ route, navigation, db, isConnected }) => {
         />
     }
     
+    
     const renderInputToolbar = (props) => {
-        return <InputToolbar 
+        if (!isConnected) return null;
+      
+        return (
+          <InputToolbar
             {...props}
             containerStyle={styles.InputToolbar}
-            accessibilityLabel='Input field'
-            accessibilityHint='Type something to send a message'
-            accessibilityRole='text' 
-        />
-    }
+            accessibilityLabel="Input field"
+            accessibilityHint="Type something to send a message"
+            accessibilityRole="text"
+          />
+        );
+    };
+      
 
     const renderSend = (props) => {
         return (
-            <Send 
+            <Send
                 {...props}
                 accessibilityLabel='Send icon button'
                 accessibilityHint='Press the icon button to send a message'
@@ -76,7 +112,7 @@ const Chat = ({ route, navigation, db, isConnected }) => {
                     <FontAwesome name="send" size={22} color="#fff" />
                 </View>
             </Send>
-        );
+        )
     }
 
     const renderComposer = (props) => {
@@ -139,7 +175,7 @@ const Chat = ({ route, navigation, db, isConnected }) => {
                 alwaysShowSend={true}
             />
             { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
-            { Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={-211} /> : null } 
+            { Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={-211} /> : null }
            
         </View>
     );
